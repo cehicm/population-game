6f77ffcca1a40e96ab06383e08be5869d7d5749c
Marija Cehic <cehicmarija@gmail.com>
2021-06-25 18:17:21 +0200

    finished basic functionality for 1 country

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,12 +1,9 @@
 const api_url = "https://restcountries.eu/rest/v2/";
+//for or forEach to handle all 250 countries
 
-fetch("https://restcountries.eu/rest/v2/")
-  .then(function (response) {
-    return response.json();
-  })
+let apiResponse = fetch(api_url)
+  .then((response) => response.json())
   .then(function (data) {
-    console.log(data[0].flag);
-
     const name = data[0].name;
     document.querySelector(".country-name").innerHTML = name;
 
@@ -15,10 +12,61 @@ fetch("https://restcountries.eu/rest/v2/")
 
     const img = document.querySelector(".country-pic");
     img.src = data[0].flag;
+
+    const population = data[0].population;
+    document.querySelector(
+      ".country-population"
+    ).innerHTML = population.toLocaleString();
+
+    //Get Player Results
+    const getResults = () => {
+      const resultSpan = document.querySelector(".results-correct");
+      //Player number input
+      let player1 = document.getElementById("player-one-input").value;
+      let player2 = document.getElementById("player-two-input").value;
+
+      //Player chosen units
+      let playerOneUnitValue = document.getElementById("player-one-unit-value");
+      let playerTwoUnitValue = document.getElementById("player-two-unit-value");
+
+      const value1 = playerOneUnitValue.value;
+      const value2 = playerTwoUnitValue.value;
+
+      const playerOneTotal = player1 * value1;
+      const playerTwoTotal = player2 * value2;
+
+      if (player1 === "" || player2 === "") {
+        resultSpan.innerHTML = "One or two empty values";
+      } else {
+        document.querySelector(
+          ".results-span"
+        ).innerHTML = `The correct number is ${population.toLocaleString()}`;
+
+        const playerWin = document.querySelector(".player-win");
+
+        if (
+          Math.abs(population - playerOneTotal) <
+          Math.abs(population - playerTwoTotal)
+        ) {
+          playerWin.innerText = "Player 1 wins!";
+        }
+
+        if (
+          Math.abs(population - playerOneTotal) >
+          Math.abs(population - playerTwoTotal)
+        ) {
+          playerWin.innerText = "Player 2 wins!";
+        }
+      }
+    };
+
+    const resultBtn = document.getElementById("result-btn");
+    resultBtn.addEventListener("click", getResults);
   });
 
-//turn response into blob
-//use the data[0].flag for src
+//get the inputs and compare them to the population number
+
+//calculate total population - player input, which one is closer to 0 is the winner
 
 // async function getCountry() {
 //   const response = await fetch(api_url);
