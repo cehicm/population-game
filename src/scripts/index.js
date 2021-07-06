63727800c66bec6da38aabee395dbb13e0d2db0a
Marija Cehic <cehicmarija@gmail.com>
2021-07-06 17:16:42 +0200

    fixed isssues with player scores

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,27 +1,29 @@
 const api_url = "https://restcountries.eu/rest/v2/";
 
 //TO DO:
-// HANDLE IT WITH A PROMISE
-// Reconfigure code to have all funcs separately; so the btn doesn't get deleted with this.clear() -- how can all of them use the same main data?
-//Get score tracking to work correctly
-//clear input field on focus
-// First screen with open btn, opaque bg
+// MAKE A START SCREEN: 
+//First screen with open btn, opaque bg
 //Clear up and reconfigure code
 //Update CSS for new layout
+//block second click for results
 
-let playerOneScore = 0;
-let playerTwoScore = 0;
 
-let apiResponse = fetch(api_url)
+//Shorten the code for getting player results
+
+let getData = fetch(api_url)
   .then((data) => data.json())
-  .then(function skipCountry(data) {
-    const skipBtn = document.getElementById("skip-btn");
-    skipBtn.addEventListener("click", skipCountry);
+  .then(function getData(data) {
+    let playerOneScore = 0;
+    let playerTwoScore = 0;
+ 
 
     function skipCountry() {
       let random = data[Math.floor(Math.random() * data.length)];
-      console.log(random);
-      console.log(random.name);
+
+      const countryPopulation = document.querySelector(".country-population");
+
+      const population = random.population;
+      countryPopulation.innerHTML = population.toLocaleString();
 
       const countryName = document.querySelector(".country-name");
       countryName.innerHTML = random.name;
@@ -32,64 +34,60 @@ let apiResponse = fetch(api_url)
       const countryFlag = document.querySelector(".country-pic");
       countryFlag.src = random.flag;
 
-      const countryPopulation = document.querySelector(".country-population");
+        //Get Player Results
+    const getShowResults = (e) => {
+ e.stopImmediatePropagation()
+      
+      let player1 = document.getElementById("player-one-input").value;
+      let player2 = document.getElementById("player-two-input").value;
+      const resultSpan = document.querySelector(".results-correct");
+      //Player number input
 
-      const population = random.population;
-      countryPopulation.innerHTML = population.toLocaleString();
+      //Player chosen units
+      let playerOneUnitValue = document.getElementById("player-one-unit-value");
+      let playerTwoUnitValue = document.getElementById("player-two-unit-value");
 
-      //Get Player Results
-      const getShowResults = () => {
-        const resultSpan = document.querySelector(".results-correct");
-        //Player number input
-        let player1 = document.getElementById("player-one-input").value;
-        let player2 = document.getElementById("player-two-input").value;
+      const value1 = playerOneUnitValue.value;
+      const value2 = playerTwoUnitValue.value;
 
-        //Player chosen units
-        let playerOneUnitValue = document.getElementById(
-          "player-one-unit-value"
-        );
-        let playerTwoUnitValue = document.getElementById(
-          "player-two-unit-value"
-        );
+      let playerOneTotal = player1 * value1;
+      let playerTwoTotal = player2 * value2;
 
-        const value1 = playerOneUnitValue.value;
-        const value2 = playerTwoUnitValue.value;
+      if (player1 === "" || player2 === "") {
+        resultSpan.innerHTML = "One or two empty values";
+      } else {
+        resultSpan.innerHTML = `The correct number is ${population.toLocaleString()}`;
+        let playerWin = document.querySelector(".results-player-win");
 
-        const playerOneTotal = player1 * value1;
-        const playerTwoTotal = player2 * value2;
-
-        if (player1 === "" || player2 === "") {
-          resultSpan.innerHTML = "One or two empty values";
-        } else {
-          resultSpan.innerHTML = `The correct number is ${population.toLocaleString()}`;
-          let playerWin = document.querySelector(".results-player-win");
-
-          if (
-            Math.abs(parseInt(population) - playerOneTotal) <
-            Math.abs(parseInt(population) - playerTwoTotal)
-          ) {
-            playerWin.innerText = "Player 1 wins!";
-            // this.remove();
-            playerOneScore++;
-          }
-
-          if (
-            Math.abs(parseInt(population) - playerOneTotal) >
-            Math.abs(parseInt(population) - playerTwoTotal)
-          ) {
-            playerWin.innerText = "Player 2 wins!";
-            // this.remove();
-
-            playerTwoScore++;
-          }
-
-          document.getElementById(
-            "current-score"
-          ).innerHTML = `P1: ${playerOneScore} --- P2: ${playerTwoScore}`;
+        if (
+          Math.abs(parseInt(population) - playerOneTotal) <
+          Math.abs(parseInt(population) - playerTwoTotal)
+        ) {
+          playerWin.innerText = "Player 1 wins!";
+          
+          playerOneScore++;
+        } 
+        
+         if (
+          Math.abs(parseInt(population) - playerOneTotal) >
+          Math.abs(parseInt(population) - playerTwoTotal)
+        ) {
+          playerWin.innerText = "Player 2 wins!";
+         
+          playerTwoScore++;
         }
-      };
-
-      const resultBtn = document.getElementById("result-btn");
-      resultBtn.addEventListener("click", getShowResults);
+        document.getElementById(
+          "current-score"
+        ).innerHTML = `P1: ${playerOneScore} --- P2: ${playerTwoScore}`;
+      }
+      
+    };
+    const resultBtn = document.getElementById("result-btn");
+    resultBtn.addEventListener("click", getShowResults);
     }
+
+  
+
+    const skipBtn = document.getElementById("skip-btn");
+    skipBtn.addEventListener("click", skipCountry);
   });
