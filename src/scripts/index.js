b3fdcd1dc4d8b40cee8189067dc8474f7a87298f
Marija Cehic <cehicmarija@gmail.com>
2021-07-08 14:29:10 +0200

    fixed functionality

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,6 +4,14 @@ const api_url = "https://restcountries.eu/rest/v2/";
 //In the future:
 // to be able to add more players
 
+//Globs
+let playerForm = document.getElementById("player-form");
+const resultBtn = document.getElementById("result-btn");
+const counterShow = document.getElementById("counter");
+
+playerOneScore = 0;
+playerTwoScore = 0;
+
 async function getData() {
   await fetch(api_url)
     .then((data) => {
@@ -21,28 +29,23 @@ async function getData() {
     });
 }
 
-playerOneScore = 0;
-playerTwoScore = 0;
-
-function skipCountry(data) {
+skipCountry = (data) => {
   let random = data[Math.floor(Math.random() * data.length)];
+  const population = random.population;
 
   document.querySelector(".country-pic").src = random.flag;
-
-  const population = random.population;
 
   document.querySelector(".country-name").innerHTML = random.name;
 
   const nativeName = document.querySelector(".country-name-native");
   nativeName.innerHTML = random.nativeName;
 
-  function getShowResults(data) {
+  function getShowResults() {
     let player1 = document.getElementById("player-one-input").value;
     let player2 = document.getElementById("player-two-input").value;
-
     const resultSpan = document.querySelector(".results-correct");
 
-    //Player chosen units
+    //Player chosen units & Total
     let playerOneUnitValue = document.getElementById("player-one-unit-value");
     let playerTwoUnitValue = document.getElementById("player-two-unit-value");
 
@@ -78,13 +81,36 @@ function skipCountry(data) {
 
       document.getElementById("current-score").innerHTML = `Current score:
       <p> P1: ${playerOneScore} </p> <p> P2: ${playerTwoScore} </p>`;
+
+      //counter function
+      let counter = 4;
+      function countdown(counter) {
+        if (counter > 0) {
+          counter--;
+          setTimeout(function () {
+            countdown(counter);
+          }, 1000);
+          counterShow.innerHTML = counter;
+        }
+        if (counter === 0) {
+          counterShow.innerHTML = "";
+        }
+      }
+
+      countdown(counter);
+
+      //Reset form
+      setTimeout(() => {
+        playerForm.reset();
+        skipCountry(data);
+        playerWin.innerText = "";
+        resultSpan.innerText = "";
+        document.querySelector(".container").classList.add("hide-me");
+      }, 3000);
     }
   }
-  const resultBtn = document.getElementById("result-btn");
+
   resultBtn.addEventListener("click", getShowResults, { once: true });
-}
+};
 
-const skipBtn = document.getElementById("skip-btn");
-skipBtn.addEventListener("click", getData, true);
-
-getData();
+resultBtn.addEventListener("click", getData(), true);
